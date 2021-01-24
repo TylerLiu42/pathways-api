81d2d5016b312cced3e5633668a59861225ed1fa
Sahibjot Saggu <sahibjot@live.ca>
2021-01-23 21:05:27 -0500

    add user_exists endpoint and cors

--- api/api.py
+++ api/api.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
-from flask_mysqldb import MySQL 
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+from flask_cors import CORS
 import sys
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 f = open("pw.txt", "r")
 pw = f.read().replace('\n','')
@@ -18,21 +20,26 @@ def sign_up():
     user = request.get_json() 
     user_id = user.get('userID')
     role = user.get('role')
-    if role != 'applicant' and role != 'employer' and role != 'mentor':
-        return "Invalid role", 400
+    if role not in ['applicant', 'employer', 'mentor']:
+        return jsonify(created=False, message="Incorrect user role."), 400
     name = user.get('name')
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT userID from Users where userID = %s", [user_id])
-    users = cur.fetchall()
-    print('rowcount: ' + str(cur.rowcount), file=sys.stderr)
-    if (cur.rowcount == 0):
-        print(role, len(role), file=sys.stderr)
+    try:
+        cur = mysql.connection.cursor()
         cur.execute("INSERT INTO Users VALUES (%s, %s, %s)", (user_id, name, role))
         mysql.connection.commit()
         cur.close()
-        return "Created new user", 200
+    except:
+        return jsonify(created=False, message="User already exists."), 400
+    return jsonify(created=True), 200
+
+@app.route('/api/user_exists', methods=['GET'])
+def user_exists():
+    user_id = request.args.get('userID')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT userID from Users where userID = %s", [user_id])
+    exists = not cur.rowcount == 0
     cur.close()
-    return "User already exists", 403
+    return jsonify(exists=exists), 200
     
 if __name__ == '__main__':
     app.run(debug=True)
